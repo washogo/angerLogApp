9e11b7d8826a2065531cbb5ed3600bec072ea02f
maruko <g031g143@gmail.com>
2024-12-29 20:57:13 +0900

    🐛 登録処理後のエラー解消

--- components/templates/TaskTemplate.tsx
+++ components/templates/TaskTemplate.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 import { Container } from "@mui/material";
 import TaskForm from "../molecules/TaskForm";
 import Header from "../organisms/Header";
 import { selectUserTaskAll, selectTaskDetail } from "@/api/task";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useEffect, useState } from "react";
 
 type TaskTemplateProps = {
   mode: "new" | "edit";
@@ -16,30 +19,45 @@ type WorkContent = {
   content: string;
 };
 
-const TaskTemplate = async ({ mode, taskId }: TaskTemplateProps) => {
-  const toastId = toast.loading("処理中・・・・。");
-  let initialCategories: string[] = [];
-  let initialData = undefined;
-  try {
-    const tasks = await selectUserTaskAll();
-    initialCategories = Array.from(
-      new Set(tasks?.map((task: WorkContent) => task.category) || [])
-    );
-    if (mode === "edit" && taskId) {
-      const taskDetail = await selectTaskDetail(taskId);
-      initialData = taskDetail ? taskDetail[0] : undefined;
-    }
-  } catch (error) {
-    const errorMessage =
-      error instanceof Error ? error.message : "不明なエラーが発生しました。";
-    toast.update(toastId, {
-      render: errorMessage,
-      type: "error",
-      isLoading: false,
-      autoClose: 5000,
-      closeOnClick: true,
-    });
-  }
+const TaskTemplate = ({ mode, taskId }: TaskTemplateProps) => {
+  const [initialCategories, setInitialCategories] = useState<string[]>([]);
+  const [initialData, setInitialData] = useState<WorkContent | undefined>(
+    undefined
+  );
+
+  useEffect(() => {
+    const fetchInitialData = async () => {
+      const toastId = toast.loading("処理中・・・・。");
+      try {
+        const tasks = await selectUserTaskAll();
+        setInitialCategories(
+          Array.from(
+            new Set(tasks?.map((task: WorkContent) => task.category) || [])
+          )
+        );
+
+        if (mode === "edit" && taskId) {
+          const taskDetail = await selectTaskDetail(taskId);
+          setInitialData(taskDetail ? taskDetail[0] : undefined);
+        }
+      } catch (error) {
+        const errorMessage =
+          error instanceof Error
+            ? error.message
+            : "不明なエラーが発生しました。";
+        toast.update(toastId, {
+          render: errorMessage,
+          type: "error",
+          isLoading: false,
+          autoClose: 5000,
+          closeOnClick: true,
+        });
+      } finally {
+        toast.dismiss(toastId);
+      }
+    };
+    fetchInitialData();
+  }, [mode, taskId]);
 
   return (
     <Container maxWidth="sm" sx={{ mt: 4 }}>
