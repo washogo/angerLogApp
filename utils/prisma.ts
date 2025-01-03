cfff02f3cf1ffeb6b0f726d3617c5a3617fa889e
maruko <g031g143@gmail.com>
2025-01-03 12:21:45 +0900

    🚀

--- utils/prisma.ts
+++ utils/prisma.ts
@@ -1,10 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ["query", "info", "warn", "error"],
-  });
+const prisma = globalThis.prisma || new PrismaClient();
 
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = prisma;
+}
+
+export default prisma;
