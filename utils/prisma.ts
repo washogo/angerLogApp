c9ea91f5afc0c31a7c7b6f0c5e5c57ebc4afc890
maruko <g031g143@gmail.com>
2025-01-03 12:10:14 +0900

    🚀

--- utils/prisma.ts
+++ utils/prisma.ts
@@ -2,13 +2,15 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma =
+const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     log: ["query", "info", "warn", "error"],
   });
-
+console.log(process.env.DATABASE_URL)
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 if (!process.env.DATABASE_URL) {
   throw new Error("Environment variable DATABASE_URL is not set");
 }
+
+export default prisma;
